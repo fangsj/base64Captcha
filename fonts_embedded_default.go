35c08e6b3db0ea328bbe9012d39c38d7f7368b8c
fangshijie <fangshj@aliyun.com>
2024-06-29 19:51:03 +0800

    增加默认字体

--- fonts_embedded_default.go
+++ fonts_embedded_default.go
@@ -5,6 +5,7 @@ import "embed"
 // defaultEmbeddedFontsFS Built-in font storage.
 //
 //go:embed fonts/*.ttf
+//go:embed fonts/*.ttc
 var defaultEmbeddedFontsFS embed.FS
 
 var DefaultEmbeddedFonts = NewEmbeddedFontsStorage(defaultEmbeddedFontsFS)
