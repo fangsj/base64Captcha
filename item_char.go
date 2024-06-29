48192dffe64fcd53a9f1316e78bf4e100bbff072
fangshijie <fangshj@aliyun.com>
2024-06-29 19:42:03 +0800

    删除不必要的代码

--- item_char.go
+++ item_char.go
@@ -217,7 +217,7 @@ func (item *ItemChar) drawText(text string, fonts []*truetype.Font) error {
 	fontWidth := item.width / len(text)
 
 	for i, s := range text {
-		fontSize := item.height * (rand.Intn(7) + 7) / 16
+		fontSize := item.height * (rand.Intn(7) + 14) / 16
 		c.SetSrc(image.NewUniform(RandDeepColor()))
 		c.SetFontSize(float64(fontSize))
 		c.SetFont(randFontFrom(fonts))
